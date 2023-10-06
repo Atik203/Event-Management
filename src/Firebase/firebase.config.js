ba24574ee8d414e5a2e5ec4feeba84f05e8911ab
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2023-10-06 20:14:42 +0600

    created project and setup firebase config,tailwind,react router

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDD0wCjh9HmSBgoTKq3IyJqnz1_d_BqT_g",
+  authDomain: "event-management-6137b.firebaseapp.com",
+  projectId: "event-management-6137b",
+  storageBucket: "event-management-6137b.appspot.com",
+  messagingSenderId: "253849049868",
+  appId: "1:253849049868:web:275b6eb36403ce1fd20729",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
